7b3f655a14ed193f1f4b4713a2fed3b5de232665
SANKETSKAPSE <sanketkapse2000@gmail.com>
2023-04-04 12:42:01 +0530

    done

---
+++ Sum_array.java
@@ -0,0 +1,20 @@
+package Day5;
+
+public class Sum_array {
+
+	public static void main(String[] args) {
+	
+		int sum=0;
+		
+		int a[]= {11,33,55,77,99};
+		
+		for(int k : a)
+		{
+			sum= sum+k;
+		}
+			System.out.println(" "+ sum);
+		
+
+	}
+
+}
